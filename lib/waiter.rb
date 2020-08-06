da172ab0b07ae4ba6b34a2f5a9877acf4c96763f
Katie Le <lethikatie@gmail.com>
2020-08-06 22:42:54 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,14 @@
 class Waiter
-
-end
+  @@all = []
+  attr_accessor :name, :yrs_of_experience
+  
+  def initialize(name, yrs_of_experience)
+    @name = name 
+    @yrs_of_experience = yrs_of_experience
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end 
+end 
